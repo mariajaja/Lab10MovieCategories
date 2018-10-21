fb6b69a7dea8c612fd07d70966b25397862d6231
Mariah Hall <hallmar94@gmail.com>
2018-10-21 15:43:58 -0400

    initial commit of lab 10 movie categories

---
+++ src/MovieCategoryApp.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author Mariah
+ *
+ */
+public class MovieCategoryApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner userInput = new Scanner(System.in);
+
+		System.out.println("Welcome to the Movie List Application!");
+		// TODO create method that stores movie list (and categories)
+		// TODO ask user what category they'd like to see list of films in a certain
+		// category (and use numbers rather than the name)
+		// TODO create method to display movie list by each category (and in
+		// alphabetical order)
+		// TODO ask user if they would like to continue
+
+	}
+
+}
---
+++ src/MovieList.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+
+/**
+ * @author Mariah
+ *
+ */
+public class MovieList {
+	private String movieTitle;
+	private String movieGenre;
+	private int genreNumber;
+
+	public MovieList() {
+
+	}
+
+	public MovieList(String movieTitle, String movieGenre) {
+		this.movieTitle = movieTitle;
+		this.movieGenre = movieGenre;
+
+	}
+
+	public String getMovieTitle() {
+		return movieTitle;
+	}
+
+	public void setMovieTitle(String movieTitle) {
+		this.movieTitle = movieTitle;
+	}
+
+	public String getMovieGenre() {
+		return movieGenre;
+	}
+
+	public void setMovieGenre(String movieGenre) {
+		this.movieGenre = movieGenre;
+	}
+
+}
