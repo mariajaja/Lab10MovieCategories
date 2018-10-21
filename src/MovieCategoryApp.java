b785d36a4a6cd10ab66a965a389e6edfdab77800
Mariah Hall <hallmar94@gmail.com>
2018-10-21 19:28:39 -0400

    final commit of lab 10 movie list

--- src/MovieCategoryApp.java
+++ src/MovieCategoryApp.java
@@ -5,40 +5,63 @@ import java.util.Scanner;
  */
 
 /**
- * @author Mariah
+ * @author Mariah Hall
  *
  */
 public class MovieCategoryApp {
 
 	/**
-	 * @param args
+	 * This program contains a list of 10 films separated into 4 genres. It asks the
+	 * user to choose a genre and then lists all of the films contained within that
+	 * genre.
 	 */
 	public static void main(String[] args) {
 		Scanner userInput = new Scanner(System.in);
 		MovieList filmList = new MovieList();
+		String userResponse;
 
 		System.out.println("Welcome to the Movie List Application!");
-		// TODO create method that stores movie list (and categories)
-		// TODO ask user what category they'd like to see list of films in a certain
-		// category (and use numbers rather than the name)
-		// TODO create method to display movie list by each category (and in
-		// alphabetical order)
-		// TODO ask user if they would like to continue
+
+		System.out.println("There are 10 movies in this list.");
+
+		// calls method to add films into the arrays
+		createMovieList();
+
+		do {
+			// method to show list of films by user's chosen category
+			MovieList.categoryList("Comedy", "SciFi", "Animated", "Drama", userInput);
+			// clears out lines after nextInt()
+			userInput.nextLine();
+
+			// ask if user wants to pick a new category
+			System.out.print("Continue? (y/n): ");
+			userResponse = userInput.nextLine();
+
+		} while (userResponse.toLowerCase().startsWith("y"));
+
+		System.out.println("Goodbye!");
 
 	}
 
+	// method that hold the list of films
 	public static void createMovieList() {
-		MovieList filmOne = new MovieList("Blazing Saddles", "comedy");
-		MovieList filmTwo = new MovieList("ex machina", "scifi");
-		MovieList filmThree = new MovieList("Hot Fuzz", "comedy");
-		MovieList filmFour = new MovieList("Moana", "animated");
-		MovieList filmFive = new MovieList("The Truman Show", "drama");
-		MovieList filmSix = new MovieList("Shrek", "animated");
-		MovieList filmSeven = new MovieList("Moonlight", "drama");
-		MovieList filmEight = new MovieList("Under the Skin", "drama");
-		MovieList filmNine = new MovieList("Serenity", "scifi");
-		MovieList filmTen = new MovieList("Anastasia", "animated");
+
+		// list of categories to be used
+		String categoryOne = "Comedy";
+		String categoryTwo = "SciFi";
+		String categoryThree = "Animated";
+		String categoryFour = "Drama";
+
+		MovieList filmOne = new MovieList("Blazing Saddles", categoryOne);
+		MovieList filmTwo = new MovieList("ex machina", categoryTwo);
+		MovieList filmThree = new MovieList("Hot Fuzz", categoryOne);
+		MovieList filmFour = new MovieList("Moana", categoryThree);
+		MovieList filmFive = new MovieList("The Truman Show", categoryFour);
+		MovieList filmSix = new MovieList("Shrek", categoryThree);
+		MovieList filmSeven = new MovieList("Moonlight", categoryFour);
+		MovieList filmEight = new MovieList("Under the Skin", categoryFour);
+		MovieList filmNine = new MovieList("Serenity", categoryTwo);
+		MovieList filmTen = new MovieList("Anastasia", categoryThree);
 
 	}
-
 }
