4dc3cffc2a036cd2003a978fec8a04610add3b68
Mariah Hall <hallmar94@gmail.com>
2018-10-22 11:35:27 -0400

    moved methods into movie list class

--- src/MovieCategoryApp.java
+++ src/MovieCategoryApp.java
@@ -1,8 +1,6 @@
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
-import java.util.TreeSet;
 
 /**
  * 
@@ -33,7 +31,7 @@ public class MovieCategoryApp {
 
 		do {
 			// method to show list of films by user's chosen category
-			categoryList("Comedy", "SciFi", "Animated", "Drama", userInput, filmList);
+			MovieList.categoryList("Comedy", "SciFi", "Animated", "Drama", userInput, filmList);
 			// clears out lines after nextInt()
 			userInput.nextLine();
 
@@ -79,50 +77,5 @@ public class MovieCategoryApp {
 
 		return filmList;
 
-	}
-
-	// list for printing out the available genres in the list (four, in this case)
-	public static void categoryList(String categoryOne, String categoryTwo, String categoryThree, String categoryFour,
-			Scanner userInput, List<MovieList> filmList) {
-		int userResponse = 0;
-		do {
-			try {
-				System.out.println("Categories: 1) " + categoryOne + " 2) " + categoryTwo + " 3) " + categoryThree
-						+ " 4) " + categoryFour);
-				System.out.print("What category are you interested in? ");
-				userResponse = userInput.nextInt();
-
-				if (userResponse == 1) {
-					// print list of categoryOne
-					returnCategoryList(categoryOne, filmList);
-				} else if (userResponse == 2) {
-					// print list of categoryTwo
-					returnCategoryList(categoryTwo, filmList);
-				} else if (userResponse == 3) {
-					// print list of categoryThree
-					returnCategoryList(categoryThree, filmList);
-				} else if (userResponse == 4) {
-					// print list of categoryFour
-					returnCategoryList(categoryFour, filmList);
-				} else {
-					System.out.println("Sorry, that's not a valid genre number. Pick a number 1-4.");
-				}
-			} catch (InputMismatchException ex) {
-				System.out.println("Sorry, that's not a valid input. Please pick a number 1-4.");
-				userInput.nextLine();
-			}
-		} while (userResponse != 1 && userResponse != 2 && userResponse != 3 && userResponse != 4);
-	}
-
-	// sorts through the list to return movie titles for specific categories
-	public static void returnCategoryList(String categoryNumber, List<MovieList> filmList) {
-		TreeSet<String> filmsInCategory = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
-		for (int i = 0; i < filmList.size(); i++) {
-			if (filmList.get(i).getMovieGenre().equals(categoryNumber)) {
-				String filmTitle = filmList.get(i).getMovieTitle();
-				filmsInCategory.add(filmTitle);
-			}
-		}
-		System.out.println(categoryNumber + " contains these films: " + filmsInCategory.toString());
 	}
 }
--- src/MovieList.java
+++ src/MovieList.java
@@ -1,3 +1,8 @@
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+import java.util.TreeSet;
+
 /**
  * 
  */
@@ -33,6 +38,51 @@ public class MovieList {
 
 	public void setMovieGenre(String movieGenre) {
 		this.movieGenre = movieGenre;
+	}
+
+	// list for printing out the available genres in the list (four, in this case)
+	public static void categoryList(String categoryOne, String categoryTwo, String categoryThree, String categoryFour,
+			Scanner userInput, List<MovieList> filmList) {
+		int userResponse = 0;
+		do {
+			try {
+				System.out.println("Categories: 1) " + categoryOne + " 2) " + categoryTwo + " 3) " + categoryThree
+						+ " 4) " + categoryFour);
+				System.out.print("What category are you interested in? ");
+				userResponse = userInput.nextInt();
+
+				if (userResponse == 1) {
+					// print list of categoryOne
+					returnCategoryList(categoryOne, filmList);
+				} else if (userResponse == 2) {
+					// print list of categoryTwo
+					returnCategoryList(categoryTwo, filmList);
+				} else if (userResponse == 3) {
+					// print list of categoryThree
+					returnCategoryList(categoryThree, filmList);
+				} else if (userResponse == 4) {
+					// print list of categoryFour
+					returnCategoryList(categoryFour, filmList);
+				} else {
+					System.out.println("Sorry, that's not a valid genre number. Pick a number 1-4.");
+				}
+			} catch (InputMismatchException ex) {
+				System.out.println("Sorry, that's not a valid input. Please pick a number 1-4.");
+				userInput.nextLine();
+			}
+		} while (userResponse != 1 && userResponse != 2 && userResponse != 3 && userResponse != 4);
+	}
+
+	// sorts through the list to return movie titles for specific categories
+	public static void returnCategoryList(String categoryNumber, List<MovieList> filmList) {
+		TreeSet<String> filmsInCategory = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
+		for (int i = 0; i < filmList.size(); i++) {
+			if (filmList.get(i).getMovieGenre().equals(categoryNumber)) {
+				String filmTitle = filmList.get(i).getMovieTitle();
+				filmsInCategory.add(filmTitle);
+			}
+		}
+		System.out.println(categoryNumber + " contains these films: " + filmsInCategory.toString());
 	}
 
 }
